857a0fcc7c9fb2df6661ca89c8a2de726b37e3a1
Erin McGee <erinmcgee1234@gmail.com>
2020-02-01 15:10:22 -0800

    Testing

---
+++ Testing/testStringConcat.py
@@ -0,0 +1,12 @@
+truth="orange"
+truth_2="man"
+truth_3="bad"
+ultimate_truth=truth+" "+truth_2+" "+truth_3
+
+print(ultimate_truth)   
+
+man="orange"
+if man=="orange":
+    print("bad")
+else:
+    print("good")
